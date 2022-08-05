b8c35cdbb73c24ed1b67ac35fc14604d85fa17d6
sukmorga <naginks@outlook.com>
2022-08-05 21:36:19 +0200

    ES6 full

---
+++ ES6/ex28.js
@@ -0,0 +1,14 @@
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer is set to false to represent an unsuccessful response from a server
+    let responseFromServer = true;
+
+    if (responseFromServer) {
+        resolve("We got the data");
+    } else {
+        reject("Data not received");
+    }
+});
+
+makeServerRequest.then(result => {
+    console.log(result);
+});
