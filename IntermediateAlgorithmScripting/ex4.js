04a3d30a1cadb333103de6405852dc5ad2869f37
sukmorga <naginks@outlook.com>
2022-08-21 14:54:40 +0200

    21

---
+++ IntermediateAlgorithmScripting/ex4.js
@@ -0,0 +1,15 @@
+function whatIsInAName(collection, source) {
+    const souceKeys = Object.keys(source);
+
+    return collection.filter(obj => {
+        for (let i = 0; i < souceKeys.length; i++) {
+            if (!obj.hasOwnProperty(souceKeys[i]) ||
+                obj[souceKeys[i]] !== source[souceKeys[i]]) {
+                return false;
+            }
+        }
+        return true;
+    });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
